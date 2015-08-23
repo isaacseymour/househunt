c8f12fb66cb394b8ddbdd4eb47e09dec0a3f2eae
Jack Franklin <jack@jackfranklin.net>
2015-08-23 20:52:13 +0100

    Removing a destination reducer test

--- test/reducers-test.js
+++ test/reducers-test.js
@@ -15,6 +15,18 @@ test('adding destination', (t) => {
   const initState = { destinations: Map() };
   const newState = addDestinationReducer(initState, addDestination('WC1X 9QZ'));
   t.equal(newState.destinations.size, 1);
+  t.deepEqual(newState.destinations.first().toJS(), Map({
+    postcode: 'WC1X 9QZ',
+  }).toJS());
+  t.end();
+});
+
+test('removing a destination', (t) => {
+  const initState = {
+    destinations: Map({ 'abc': Map() }),
+  };
+  const newState = deleteDestinationReducer(initState, deleteDestination('abc'));
+  t.ok(newState.destinations.isEmpty());
   t.end();
 });
 
