5f6f54ddb7a7eba4cc6aa9921e58f26659c2d96c
Isaac Seymour <i.seymour@oxon.org>
2015-09-13 21:08:24 +0100

    Also add directions service, for getting travel times

--- app/services/geocode.js
+++ app/services/geocode.js
@@ -1,11 +1,13 @@
 // Google gets loaded in index.html, so is always on the global scope here.
-const { LatLng, GeocoderStatus } = google.maps;
+export const { LatLng } = google.maps;
+const { GeocoderStatus } = google.maps;
 const Geocoder = new google.maps.Geocoder();
 
+// Returns a promise which resolves to a LatLng
 export default function geocode(address) {
   return new Promise((resolve, reject) => {
     Geocoder.geocode({ address }, (results, status) => {
-      if(status == GeocoderStatus.OK) {
+      if(status === GeocoderStatus.OK) {
         resolve(results[0].geometry.location);
       } else {
         reject(`Google returned error ${status}`);
